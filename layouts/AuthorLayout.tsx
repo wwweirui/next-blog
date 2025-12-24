8d52c5b97737e46f34ac576dd99af5ed23db94cb
叶伟睿 <yewr@tebon.com.cn>
2025-12-24 09:29:56 +0800

    Remove unused tags and commented code
    Deleted 'nextjs' and 'react' tags from tag-data.json, leaving only 'next-js'. Also removed commented-out code from AuthorLayout.tsx for cleaner code.

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -41,13 +41,8 @@ export default function AuthorLayout({ children, content }: Props) {
               {/* <SocialIcon kind="bluesky" href={bluesky} /> */}
             </div>
           </div>
-          {/* <div className="prose dark:prose-invert max-w-none pt-8 pb-8 xl:col-span-2">
-            {children}
-          </div> */}
-          
         </div>
       </div>
     </>
   )
 }
-// 
